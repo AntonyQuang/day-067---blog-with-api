b1370e35c5b4aa6dc671f4bdf64ad5e9dd4a350c
Antony Quang <antony.quang@googlemail.com>
2022-05-17 21:27:17 +0100

    blog completed

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
+from datetime import datetime
 
 
 ## Delete this code:
@@ -67,12 +68,60 @@ def contact():
 
 
 @app.route("/new-post", methods=["GET", "POST"])
-def new_post():
+def create_new_post():
     form = CreatePostForm()
     if form.validate_on_submit():
         data = form.data
+        title = data["title"]
+        subtitle = data["subtitle"]
+        author = data["author"]
+        img_url = data["img_url"]
+        body = data["body"]
+        date = datetime.now().strftime("%B %d, %Y")
+        new_post = BlogPost(
+            title=title,
+            subtitle=subtitle,
+            date=date,
+            body=body,
+            author=author,
+            img_url=img_url
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('get_all_posts'))
     return render_template('make-post.html', form=form)
 
+
+
+@app.route('/edit-post/<int:post_id>', methods=["GET", "POST"])
+def edit_post(post_id):
+    post = db.session.query(BlogPost).get(post_id)
+
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.img_url.data
+        post.author = edit_form.author.data
+        post.body = edit_form.body.data
+        db.session.commit()
+        return redirect(url_for('show_post', index=post.id))
+    return render_template('make-post.html', form=edit_form, is_edit=True)
+
+
+@app.route('/delete-post/<int:post_id>', methods=["GET","POST"])
+def delete_post(post_id):
+    post = db.session.query(BlogPost).get(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('get_all_posts'))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
